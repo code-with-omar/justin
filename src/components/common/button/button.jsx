480a7723750bd34ad91b25d39da887f01c9146ae
code-with-omar <omarfaruk65142@gmail.com>
2025-01-05 17:39:13 +0600

    Fixed the slider error

--- src/components/common/button/button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-export default function Button() {
-  return (
-    <button className="custom-button custom-button.outlined">Click Me</button>
-  );
-}
--- src/screens/constants/search.jsx
+++ src/screens/constants/search.jsx
@@ -3,8 +3,6 @@ import Logo from "../../assets/new-logo-trans.png"; // Make sure the path is cor
 import { useModal } from "../../contexts/Modal";
 import { ArrowForward } from "../../components/common/svgs"; // Assuming ArrowForward is an SVG component
 
-import Button from "../../components/common/button/button";
-
 export default function Search({
   setIsAdvSearch,
   isAdvSearch,
@@ -42,52 +40,55 @@ export default function Search({
   };
 
   return (
-    <div className="search-page-wrapper flex flex-col justify-center items-center border-0 overflow-x-hidden overflow-y-scroll p-0 w-full h-full md:border-0 md:overflow-y-hidden xl:flex-col">
-      <div className="mb-2 flex justify-center">
-        {/* Correct way to render the image */}
-        <img className="w-56" src={Logo} alt="Logo" />
-      </div>
-
-      <h4 className="text-[#343434] text-center mb-4 font-extrabold text-xl mt-2 uppercase">
-        SEARCH FOR YOUR
-        <span className="text-[#1cbcba] text-xl font-extrabold"> COLOR</span>
-      </h4>
-
-      <form
-        id="advanced-search-form"
-        className="flex flex-col sm:flex-row items-center justify-center"
-        onSubmit={(e) => e.preventDefault()} // Prevent page reload on form submit
-      >
-        <div className="form-control flex flex-row items-center bg-background p-2 rounded-xl">
-          {/* Text Input */}
-          <input
-            type="text"
-            name="q"
-            id="query"
-            onChange={handleInputChange}
-            value={searchInput}
-            placeholder="Search by color name or code (e.g. LY7C or Nardo)"
-            className="input input-bordered bg-background text-sm placeholder:text-xs sm:w-[400px] md:text-base md:placeholder:text-sm w-64 h-12 focus:outline-none border-none"
-          />
-
-          {/* Search Button */}
-          <button
-            type="button"
-            className=" bg-primary hover:bg-primary-focus rounded-full w-10 h-10 ml-2 flex items-center justify-center"
-            onClick={handleSearch}
-          >
-            <ArrowForward className="w-6 h-6 text-white" />
-          </button>
+    <div className="search-page-wrapper flex flex-col justify-center items-center border-0 overflow-x-hidden overflow-y-scroll p-0  w-full h-full md:border-0 md:overflow-y-hidden xl:flex-col -mt-10">
+      <div className="">
+        <div className="mb-2 flex justify-center">
+          {/* Correct way to render the image */}
+          <img className="w-56" src={Logo} alt="Logo" />
         </div>
-      </form>
 
-      <div className="flex w-full items-center pt-2 gap-2 md:mt-6">
-        <div className="flex w-full gap-2 mt-2">
-          <button className="button-primary outlined-button-primary btn">
+        <h4 className="text-[#343434] text-center mb-4 font-extrabold text-xl mt-3 uppercase">
+          SEARCH FOR YOUR
+          <span className="text-[#1cbcba] text-xl font-extrabold"> COLOR</span>
+        </h4>
+
+        <form
+          id="advanced-search-form"
+          className="flex flex-col sm:flex-row items-center justify-center"
+          onSubmit={(e) => e.preventDefault()} // Prevent page reload on form submit
+        >
+          <div className="form-control flex flex-row items-center bg-background p-2 rounded-xl">
+            {/* Text Input */}
+            <input
+              type="text"
+              name="q"
+              id="query"
+              onChange={handleInputChange}
+              value={searchInput}
+              placeholder="Search by color name or code (e.g. LY7C or Nardo)"
+              className="input input-bordered bg-background text-sm placeholder:text-xs sm:w-[400px] md:text-base md:placeholder:text-sm w-64 h-12 focus:outline-none border-none"
+            />
+
+            {/* Search Button */}
+            <button
+              type="button"
+              className=" bg-primary hover:bg-primary-focus rounded-full w-10 h-10 ml-2 flex items-center justify-center"
+              onClick={handleSearch}
+            >
+              <ArrowForward className="w-6 h-6 text-white" />
+            </button>
+          </div>
+        </form>
+
+        <div className="flex items-center pt-3 gap-2 md:mt-6 w-full justify-between">
+          {/* Filled Button */}
+          <button className="btn rounded-full bg-[#0d1120] text-white py-2 font-normal capitalize border border-black hover:bg-white hover:text-[#1cbcba] hover:border-[#1cbcba] text-sm md:text-base w-1/2">
             Search By Make
           </button>
-          <button className="button-secondary outlined-secondary btn">
-            Search By Make
+
+          {/* Outlined Button */}
+          <button className="btn rounded-full bg-white text-[#0d1120] py-2 font-normal capitalize border border-black w-1/2 hover:bg-[#1cbcba] hover:text-white hover:border-[#1cbcba] text-sm md:text-base">
+            Search Tips
           </button>
         </div>
       </div>
